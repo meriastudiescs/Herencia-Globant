2bb7f9edcbd16104d72cbdd50df0fe220714d2ab
Maria Fernanda <pt74087083@idat.edu.pe>
2023-09-11 18:36:29 -0500

    commit

--- Ejercicio03/Ejercicio03/src/Main/Main.java
+++ Ejercicio03/Ejercicio03/src/Main/Main.java
@@ -16,14 +16,10 @@ public class Main {
     public static void main(String[] args) {
         
         ArrayList<Electrodomestico> electrodomesticos = new ArrayList<>();
-        
-        // Crear y agregar 4 electrodomésticos al ArrayList
-        electrodomesticos.add(new Lavadora(2000, "blanco", 'A', 30, 15)); // Lavadora con precio y carga
-        electrodomesticos.add(new Televisor(5000, "rojo", 'C', 42, 23, true)); // Televisor con precio, pulgadas y eficiencia
-        electrodomesticos.add(new Lavadora(1500, "rojo", 'B', 34, 20)); // Otra lavadora
-        electrodomesticos.add(new Televisor(4000, "negro", 'B', 32, 40, true)); // Otro televisor
-
-        // Calcular y mostrar el precio final de cada electrodoméstico
+        electrodomesticos.add(new Lavadora(2000, "blanco", 'A', 30, 15)); 
+        electrodomesticos.add(new Televisor(5000, "rojo", 'C', 42, 23, true)); 
+        electrodomesticos.add(new Lavadora(1500, "rojo", 'B', 34, 20)); 
+        electrodomesticos.add(new Televisor(4000, "negro", 'B', 32, 40, true)); 
         double precioTotalElectrodomesticos = 0;
         double precioTotalLavadoras = 0;
         double precioTotalTelevisores = 0;
@@ -38,12 +34,8 @@ public class Main {
             } else if (electrodomestico instanceof Televisor) {
                 precioTotalTelevisores += electrodomestico.precioFinal();
             }
-        }
-
-        // Mostrar el precio total de todos los electrodomésticos
+        }        
         System.out.println("Precio total de todos los electrodomésticos: $" + precioTotalElectrodomesticos);
-
-        // Mostrar el precio total de las lavadoras y los televisores
         System.out.println("Precio total de las lavadoras: $" + precioTotalLavadoras);
         System.out.println("Precio total de los televisores: $" + precioTotalTelevisores);
     }
